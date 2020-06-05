71bdc22a4921e2de81159076d32b10f36aca66f7
ricardo-farias <ricardo.farias@thoughtworks.com>
2020-06-05 13:05:14 -0500

    [Ricardo] Create to covid buckets, and two covid team members. One for Us data, and another for Italy data with their respective policies

--- aws-iam.tf
+++ aws-iam.tf
@@ -1,33 +1,33 @@
-resource "aws_iam_group" "data-scientists-group" {
-  name = "data-scientists"
+resource "aws_iam_group" "covid-italy-team-group" {
+  name = "covid-italy-team"
 }
 
-resource "aws_iam_group" "covid-team-group" {
-  name = "covid-team"
+resource "aws_iam_group" "covid-us-team-group" {
+  name = "covid-us-team"
 }
 
-resource "aws_iam_user" "covid-team-member-user" {
-  name = "covid-member1"
+resource "aws_iam_user" "covid-us-team-member-user" {
+  name = "covid-us-member1"
 }
 
-resource "aws_iam_user" "data-scientist-team-user" {
-  name = "data-scientist-member1"
+resource "aws_iam_user" "covid-italy-team-member-user" {
+  name = "covid-italy-member1"
 }
 
 resource "aws_iam_user_group_membership" "covid-team-assignment" {
-  user = aws_iam_user.covid-team-member-user.name
+  user = aws_iam_user.covid-us-team-member-user.name
 
   groups = [
-    aws_iam_group.covid-team-group.name,
+    aws_iam_group.covid-us-team-group.name,
   ]
 }
 
-resource "aws_iam_user_group_membership" "data-scientist-assignment" {
-  user   = aws_iam_user.data-scientist-team-user.name
-  groups = [aws_iam_group.data-scientists-group.name]
+resource "aws_iam_user_group_membership" "covid-italy-team-assignment" {
+  user   = aws_iam_user.covid-italy-team-member-user.name
+  groups = [aws_iam_group.covid-italy-team-group.name]
 }
 
 output "output" {
-  value = [aws_iam_group.covid-team-group.arn, aws_iam_group.data-scientists-group.arn]
+  value = [aws_iam_group.covid-us-team-group.arn, aws_iam_group.covid-italy-team-group.arn]
 }
 
--- aws-s3.tf
+++ aws-s3.tf
@@ -1,14 +1,21 @@
-resource "aws_s3_bucket" "covid-bucket" {
-  bucket = "data-mesh-covid-domain"
+resource "aws_s3_bucket" "covid-us-bucket" {
+  bucket = "data-mesh-covid-us-domain"
   acl    = "private"
-
   tags = {
     Environment = "Dev"
   }
 }
 
-resource "aws_s3_bucket_policy" "b" {
-  bucket = aws_s3_bucket.covid-bucket.id
+resource "aws_s3_bucket" "covid-italy-bucket" {
+  bucket = "data-mesh-covid-italy-domain"
+  acl  = "private"
+  tags = {
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_policy" "covid-us-bucket-policy" {
+  bucket = aws_s3_bucket.covid-us-bucket.id
 
   policy = <<POLICY
 {
@@ -18,9 +25,29 @@ resource "aws_s3_bucket_policy" "b" {
     {
       "Sid": "AllowCovidTeamMember",
       "Effect": "Allow",
-      "Principal": { "AWS" : "${aws_iam_user.covid-team-member-user.arn}" },
+      "Principal": { "AWS" : "${aws_iam_user.covid-us-team-member-user.arn}" },
       "Action": "s3:*",
-      "Resource": "${aws_s3_bucket.covid-bucket.arn}/*"
+      "Resource": "${aws_s3_bucket.covid-us-bucket.arn}/*"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_s3_bucket_policy" "covid-italy-bucket-policy" {
+  bucket = aws_s3_bucket.covid-italy-bucket.id
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Id": "COVIDBUCKETPOLICY",
+  "Statement": [
+    {
+      "Sid": "AllowCovidTeamMember",
+      "Effect": "Allow",
+      "Principal": { "AWS" : "${aws_iam_user.covid-italy-team-member-user.arn}" },
+      "Action": "s3:*",
+      "Resource": "${aws_s3_bucket.covid-italy-bucket.arn}/*"
     }
   ]
 }
