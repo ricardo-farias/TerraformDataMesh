ecaa47f69c8f49c0c0cc8fc494a65f1d8b47b5c9
ricardo-farias <ricardo.farias@thoughtworks.com>
2020-07-14 15:03:11 -0500

    [Ricardo] [Refactor] old Dockerfile code was removed, added packages and versions to requirements.txt

--- airflow/dags/airflow_variables.py
+++
@@ -1,5 +0,0 @@
-from airflow.models import Variable
-
-
-aws_access_key_id = Variable.get("aws_access_key_id"),
-aws_secret_access_key = Variable.get("aws_secret_access_key")
--- airflow/scripts/deploy-to-ecr.sh
+++ airflow/scripts/deploy-to-ecr.sh
@@ -1,4 +1,4 @@
 aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 150222441608.dkr.ecr.us-east-2.amazonaws.com
-docker build -t airflow-orchestrator .
-docker tag airflow-orchestrator:latest 150222441608.dkr.ecr.us-east-2.amazonaws.com/airflow-orchestrator:latest
-docker push 150222441608.dkr.ecr.us-east-2.amazonaws.com/airflow-orchestrator:latest
+docker build -t airflow .
+docker tag airflow:latest 150222441608.dkr.ecr.us-east-2.amazonaws.com/airflow:latest
+docker push 150222441608.dkr.ecr.us-east-2.amazonaws.com/airflow:latest
