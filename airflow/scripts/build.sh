58aaebfb0be3f476ae147d0481ffade6d9e2ca2a
ricardo-farias <ricardo.farias@thoughtworks.com>
2020-07-23 10:48:29 -0500

    [Ricardo] Modified deploy script, requires an aws location to execute. Added a build script without deploying for local builds

---
+++ airflow/scripts/build.sh
@@ -0,0 +1,2 @@
+docker build -t airflow .
+docker tag airflow:latest 150222441608.dkr.ecr.us-east-2.amazonaws.com/airflow:latest
--- airflow/scripts/deploy-to-ecr.sh
+++ airflow/scripts/deploy-to-ecr.sh
@@ -1,4 +1,13 @@
-aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 150222441608.dkr.ecr.us-east-2.amazonaws.com
-docker build -t airflow .
-docker tag airflow:latest 150222441608.dkr.ecr.us-east-2.amazonaws.com/airflow:latest
-docker push 150222441608.dkr.ecr.us-east-2.amazonaws.com/airflow:latest
+if [ $# -gt 0 ]; then
+
+  aws_account_location=$1
+
+  aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin "${aws_account_location}"
+  docker build -t airflow .
+  docker tag airflow:latest "${aws_account_location}"/airflow:latest
+  docker push "${aws_account_location}"/airflow:latest
+
+else
+  echo "Must provide an aws location: ***********.dkr.ecr.us-east-2.amazonaws.com"
+
+fi
