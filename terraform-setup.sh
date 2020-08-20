a95a1221a3a4b515f6d32c5dbc38941a8659230a
Nelson Liao <nliao@Nelsons-MacBook-Pro.local>
2020-08-20 10:26:28 -0700

    [Aayush/Nelson] Trimmed addition line character upon running script to fernet.txt and rds_password.txt

--- terraform-setup.sh
+++ terraform-setup.sh
@@ -1,2 +1,2 @@
-(python -c "from cryptography.fernet import Fernet; FERNET_KEY = Fernet.generate_key().decode(); print(FERNET_KEY)") > fernet.txt
-openssl rand -base64 32 > rds_password.txt
+(python -c "from cryptography.fernet import Fernet; FERNET_KEY = Fernet.generate_key().decode(); print(FERNET_KEY)") | tr -d '\n' > fernet.txt
+openssl rand -base64 32 | tr -d '\n' > rds_password.txt
