815b31a6ce5754b9231658e2d948030dafdf04e8
Rebecca Green <rebecca.green@thoughtworks.com>
2020-09-22 16:33:59 -0400

    [#52 - Aaron/Reb] Changed s3 bucket naming convention

--- terraform/modules/s3/outputs.tf
+++ terraform/modules/s3/outputs.tf
@@ -6,6 +6,14 @@ output "bike-bucket" {
   value = aws_s3_bucket.bike-bucket.bucket
 }
 
+output "covid-data-bucket_arn" {
+  value = aws_s3_bucket.covid-data-bucket.arn
+}
+
+output "bike-bucket_arn" {
+  value = aws_s3_bucket.bike-bucket.arn
+}
+
 output "logging-bucket" {
   value = aws_s3_bucket.logging-bucket.bucket
 }
