d7621113a3f8223ca828a0bcbaec9c59de907b31
AlexPerson <alex.person@thoughtworks.com>
2020-09-24 14:25:57 -0500

    [AP] Updates/adds scripts to build and run on local K8s

--- docker/python_aws/scripts/buildLocal.sh
+++ docker/python_aws/scripts/buildLocal.sh
@@ -1,3 +1,3 @@
-docker build -t python-aws .
+docker build --no-cache -t python-aws docker/python_aws/
 docker tag python-aws localhost:5000/python-aws:latest
 docker push localhost:5000/python-aws:latest
